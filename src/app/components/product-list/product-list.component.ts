6a2e7d27f70a6efbff8ea08ed6fefb59c6042092
Jovana Todorovic <jovana.todorovic@softelm.com>
2022-09-14 08:12:43 +0200

    adding search and filter by category functionality

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,30 +1,71 @@
 import { Component, OnInit } from '@angular/core';
-import {ApiService} from "../../sahred/services/api.service";
-import {ProductsService} from "../../sahred/services/products.service";
-import {select, Store} from "@ngrx/store";
-import {State} from "../../sahred/store";
-import {getAllPoducts} from "../../sahred/store/product.action";
-import {getAllProducts} from "../../sahred/store/product.selectors";
-import {FormControl, FormGroup} from "@angular/forms";
+import { select, Store } from '@ngrx/store';
+import { State } from '../../sahred/store';
+import { getCategories } from '../../sahred/store/product.action';
+import {
+  getAllCategories,
+  getAllProducts,
+} from '../../sahred/store/product.selectors';
+import { Observable } from 'rxjs';
+import { Product } from 'src/app/sahred/models/product.model';
+import { ProductsService } from 'src/app/sahred/services/products.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.scss']
+  styleUrls: ['./product-list.component.scss'],
 })
 export class ProductListComponent implements OnInit {
-   public allProducts:any;
+  public allProducts!: Product[];
+  public searchWord: string = '';
+  private originalArray!: Product[];
+  public filterOption!: string;
+  public categories$!: Observable<string[]>;
 
-   constructor(private apiService: ApiService, private store: Store<State>) { }
+  constructor(
+    private store: Store<State>,
+    private productService: ProductsService
+  ) {}
 
   ngOnInit(): void {
-    this.store.pipe(select(getAllProducts)).subscribe(data=>{
-      this.allProducts =data;
-    })
-
+    this.store.pipe(select(getAllProducts)).subscribe((data) => {
+      this.allProducts = data;
+      this.originalArray = data;
+    });
+    this.getCategories();
+    this.categories$ = this.store.pipe(select(getAllCategories));
   }
 
+  public searchByValue() {
+    const searchWord = this.searchWord;
+    if (searchWord === '') {
+      this.allProducts = this.originalArray;
+      return;
+    }
+    this.allProducts = this.allProducts.filter((item: Product) => {
+      return item.title
+        .toLowerCase()
+        .includes(searchWord.trim().toLocaleLowerCase());
+    });
+  }
 
+  public filter() {
+    this.allProducts = this.originalArray;
+    if (this.filterOption === undefined) {
+      return;
+    }
+    this.allProducts = this.allProducts.filter((item: Product) => {
+      return item.category
+        .toLowerCase()
+        .includes(this.filterOption.trim().toLocaleLowerCase());
+    });
+  }
 
+  private getCategories() {
+    this.store.dispatch(getCategories());
+  }
 
+  public onAddToBasket(product: any) {
+    this.productService.add(product);
+  }
 }
